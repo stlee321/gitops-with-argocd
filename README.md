464ec472ae48665b7cb26b1159b16f27c5d212ef
stlee321 <lstdess321@gmail.com>
2024-01-08 09:43:16 +0900

    react-app 추가

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gitops-with-argocd
+
 Learning GitOps using ArgoCD with App of Apps pattern
+
+## React-app 추가
---
+++ react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>v1.0.0</h1>
+    </>
+  );
+}
+
+export default App;
